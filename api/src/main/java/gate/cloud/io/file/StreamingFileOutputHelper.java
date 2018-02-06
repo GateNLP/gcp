048229a67b6432ad0963059d0db642c90bdc6b86
Ian Roberts <i.roberts@dcs.shef.ac.uk>
2018-02-06 09:43:12 +0000

    Factored out the useful streaming-specific bits of JSONStreamingOutputHandler into a helper class in the api package, which other similar handlers can then use in the same way.

---
+++ api/src/main/java/gate/cloud/io/file/StreamingFileOutputHelper.java
@@ -0,0 +1,251 @@
+/*
+ *  SteamingFileOutputHelper.java
+ *  Copyright (c) 2007-2018, The University of Sheffield.
+ *
+ *  This file is part of GCP (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Affero General Public License,
+ *  Version 3, November 2007.
+ */
+package gate.cloud.io.file;
+
+import static gate.cloud.io.IOConstants.PARAM_BATCH_FILE_LOCATION;
+import static gate.cloud.io.IOConstants.PARAM_CHUNK_SIZE;
+import static gate.cloud.io.IOConstants.PARAM_COMPRESSION;
+import static gate.cloud.io.IOConstants.PARAM_NAMING_STRATEGY;
+import static gate.cloud.io.IOConstants.PARAM_PATTERN;
+import static gate.cloud.io.IOConstants.VALUE_COMPRESSION_GZIP;
+import static gate.cloud.io.IOConstants.VALUE_COMPRESSION_NONE;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.ProcessBuilder.Redirect;
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
+import java.util.zip.GZIPOutputStream;
+
+import org.apache.log4j.Logger;
+
+import gate.Gate;
+import gate.cloud.batch.DocumentID;
+import gate.cloud.io.IOConstants;
+import gate.util.GateException;
+
+/**
+ * Helper for streaming output handlers that want to write to a series of chunk files.
+ *
+ * @param <TItem> the type of the items that will be streamed to this helper.
+ * @param <TWriter> the type of the object that will be used to write items to the files.
+ */
+public class StreamingFileOutputHelper<TItem, TWriter extends AutoCloseable> {
+  private static final Logger logger =
+      Logger.getLogger(StreamingFileOutputHelper.class);
+
+  protected String pattern;
+
+  protected long chunkSize = -1L;
+
+  protected String compression;
+
+  protected File batchDir;
+
+  protected NamingStrategy namingStrategy;
+
+  protected BlockingQueue<TItem> results = new ArrayBlockingQueue<>(100);
+
+  protected ExecutorService processWaiter = Executors.newCachedThreadPool();
+
+  protected TItem endOfData;
+
+  protected Function<OutputStream, TWriter> openWriter;
+
+  protected BiConsumer<TWriter, TItem> writeItem;
+
+  protected ToIntFunction<TItem> itemSize;
+
+  /**
+   * Construct a streaming output helper.
+   * 
+   * @param endOfData
+   *          flag object that will be used to signal the end of the data
+   *          stream. This will be compared by reference and should be an object
+   *          that will not otherwise be used in normal processing.
+   * @param openWriter
+   *          function that takes an output stream and creates an appropriate
+   *          writer object for the item type.
+   * @param writeItem consumer that writes the given item to the given writer
+   * @param itemSize function that computes an approcimate size in bytes of the
+   *          given item, used to determine when to check for chunk roll-over.
+   */
+  public StreamingFileOutputHelper(TItem endOfData,
+      Function<OutputStream, TWriter> openWriter,
+      BiConsumer<TWriter, TItem> writeItem, ToIntFunction<TItem> itemSize) {
+    super();
+    this.endOfData = endOfData;
+    this.openWriter = openWriter;
+    this.writeItem = writeItem;
+    this.itemSize = itemSize;
+  }
+
+  public void config(Map<String, String> configData)
+      throws IOException, GateException {
+    String batchFileStr = configData.get(PARAM_BATCH_FILE_LOCATION);
+    if(batchFileStr != null) {
+      batchDir = new File(batchFileStr).getParentFile();
+    }
+    // naming strategy
+    String namingStrategyClassName = configData.get(PARAM_NAMING_STRATEGY);
+    if(namingStrategyClassName == null
+        || namingStrategyClassName.length() == 0) {
+      namingStrategyClassName = SimpleNamingStrategy.class.getName();
+    }
+    try {
+      Class<? extends NamingStrategy> namingStrategyClass =
+          Class.forName(namingStrategyClassName, true, Gate.getClassLoader())
+              .asSubclass(NamingStrategy.class);
+      namingStrategy = namingStrategyClass.newInstance();
+      namingStrategy.config(true, configData);
+    } catch(Exception e) {
+      throw new GateException("Could not instantiate specified naming strategy",
+          e);
+    }
+    pattern = configData.get(PARAM_PATTERN);
+    if(pattern == null) {
+      pattern = "part-%03d";
+    }
+    String chunkSizeStr = configData.get(PARAM_CHUNK_SIZE);
+    try {
+      chunkSize = Long.parseLong(chunkSizeStr);
+    } catch(Exception e) {
+      logger.info("Using default chunk size");
+      chunkSize = 99000000;
+    }
+    // get the compression value
+    compression = configData.get(PARAM_COMPRESSION);
+    if(compression == null) {
+      // default
+      compression = IOConstants.VALUE_COMPRESSION_NONE;
+    }
+  }
+
+  public void init() throws IOException, GateException {
+    // TODO Auto-generated method stub
+    new Thread(new StreamOutputter()).start();
+  }
+
+  public void sendItem(TItem item) {
+    try {
+      results.put(item);
+    } catch(InterruptedException e) {
+      Thread.currentThread().interrupt();
+    }
+  }
+
+  public void close() throws IOException, GateException {
+    try {
+      results.put(endOfData);
+    } catch(InterruptedException e) {
+      Thread.currentThread().interrupt();
+    }
+  }
+
+  protected class StreamOutputter implements Runnable {
+    private File currentFile;
+
+    private int currentChunk = -1;
+
+    private TWriter currentOutput;
+
+    private Process currentProcess;
+
+    public void run() {
+      TItem item = null;
+      try {
+        try {
+          int bytesSinceLastCheck = 0;
+          while((item = results.take()) != endOfData) {
+            if(currentOutput == null) {
+              try {
+                openNextChunk();
+              } catch(IOException e) {
+                logger.error("Failed to open output file", e);
+              }
+            }
+            writeItem.accept(currentOutput, item);
+            bytesSinceLastCheck += itemSize.applyAsInt(item);
+            if(bytesSinceLastCheck > 1024 * 1024) {
+              if(currentFile.length() > chunkSize) {
+                closeChunk();
+              }
+              bytesSinceLastCheck = 0;
+            }
+          }
+        } finally {
+          closeChunk();
+          processWaiter.shutdown();
+        }
+      } catch(InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+    }
+
+    private void closeChunk() {
+      if(currentOutput != null) {
+        try {
+          currentOutput.close();
+        } catch(Exception e) {
+          logger.warn("Error closing file " + currentFile.getAbsolutePath(), e);
+        }
+        if(currentProcess != null) {
+          final Process p = currentProcess;
+          processWaiter.execute(new Runnable() {
+            public void run() {
+              try {
+                p.waitFor();
+              } catch(InterruptedException e) {
+                logger.warn("Interrupted while waiting for process", e);
+                Thread.currentThread().interrupt();
+              }
+            }
+          });
+          currentProcess = null;
+        }
+        currentOutput = null;
+        currentFile = null;
+      }
+    }
+
+    private void openNextChunk() throws IOException {
+      // if we're restarting we might have to skip some batches
+      do {
+        String newFileName = String.format(pattern, ++currentChunk);
+        currentFile = namingStrategy.toFile(new DocumentID(newFileName));
+      } while(currentFile.exists());
+      OutputStream newStream = null;
+      if(VALUE_COMPRESSION_GZIP.equals(compression)) {
+        newStream = new GZIPOutputStream(new FileOutputStream(currentFile));
+      } else if(compression == null
+          || VALUE_COMPRESSION_NONE.equals(compression)) {
+        newStream = new FileOutputStream(currentFile);
+      } else {
+        // treat compression value as a command line
+        ProcessBuilder pb =
+            new ProcessBuilder(compression.trim().split("\\s+"));
+        pb.directory();
+        pb.redirectInput(Redirect.PIPE);
+        pb.redirectOutput(currentFile);
+        pb.redirectError(Redirect.INHERIT);
+        currentProcess = pb.start();
+        newStream = currentProcess.getOutputStream();
+      }
+      currentOutput = openWriter.apply(newStream);
+    }
+  }
+}
--- impl/src/main/java/gate/cloud/io/json/JSONStreamingOutputHandler.java
+++ impl/src/main/java/gate/cloud/io/json/JSONStreamingOutputHandler.java
@@ -11,30 +11,17 @@
  */
 package gate.cloud.io.json;
 
-import static gate.cloud.io.IOConstants.PARAM_BATCH_FILE_LOCATION;
-import static gate.cloud.io.IOConstants.PARAM_CHUNK_SIZE;
-import static gate.cloud.io.IOConstants.PARAM_PATTERN;
-import static gate.cloud.io.IOConstants.VALUE_COMPRESSION_GZIP;
-import static gate.cloud.io.IOConstants.VALUE_COMPRESSION_NONE;
-
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.lang.ProcessBuilder.Redirect;
 import java.util.Map;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.zip.GZIPOutputStream;
 
 import org.apache.log4j.Logger;
 
 import gate.Document;
 import gate.cloud.batch.DocumentID;
 import gate.cloud.io.file.JSONOutputHandler;
+import gate.cloud.io.file.StreamingFileOutputHelper;
 import gate.util.GateException;
 
 /**
@@ -50,46 +37,38 @@ public class JSONStreamingOutputHandler extends JSONOutputHandler {
   private static final Logger logger = Logger
           .getLogger(JSONStreamingOutputHandler.class);
 
-  protected String pattern;
+  private StreamingFileOutputHelper<byte[], OutputStream> helper;
 
-  protected long chunkSize = -1L;
+  public JSONStreamingOutputHandler() {
+    helper = new StreamingFileOutputHelper<byte[], OutputStream>(
+        new byte[0],
+        // we use the output stream as-is
+        x -> x,
+        (OutputStream os, byte[] bytes) -> {
+          try {
+            os.write(bytes);
+            os.flush();
+          } catch(IOException e) {
+            logger.warn("Error writing to file", e);
+          }
+        },
+        (byte[] bytes) -> bytes.length);
+  }
   
-  protected File batchDir;
-
-  private static final byte[] END_OF_DATA = new byte[0];
-
   @Override
   protected void configImpl(Map<String, String> configData) throws IOException,
           GateException {
     // TODO Auto-generated method stub
     super.configImpl(configData);
-    String batchFileStr = configData.get(PARAM_BATCH_FILE_LOCATION);
-    if(batchFileStr != null) {
-      batchDir = new File(batchFileStr).getParentFile();
-    }
-    pattern = configData.get(PARAM_PATTERN);
-    if(pattern == null) {
-      pattern = "part-%03d";
-    }
-    String chunkSizeStr = configData.get(PARAM_CHUNK_SIZE);
-    try {
-      chunkSize = Long.parseLong(chunkSizeStr);
-    } catch(Exception e) {
-      logger.info("Using default chunk size");
-      chunkSize = 99000000;
-    }
+    helper.config(configData);
   }
 
-  protected ExecutorService processWaiter = Executors.newCachedThreadPool();
-  
   protected ThreadLocal<ByteArrayOutputStream> baos =
           new ThreadLocal<ByteArrayOutputStream>() {
             protected ByteArrayOutputStream initialValue() {
               return new ByteArrayOutputStream();
             }
           };
-
-  protected BlockingQueue<byte[]> results = new ArrayBlockingQueue<byte[]>(100);
 
   @Override
   protected void outputDocumentImpl(Document document, DocumentID documentId)
@@ -98,11 +77,7 @@ public class JSONStreamingOutputHandler extends JSONOutputHandler {
     baos.get().write('\n');
     byte[] result = baos.get().toByteArray();
     baos.get().reset();
-    try {
-      results.put(result);
-    } catch(InterruptedException e) {
-      Thread.currentThread().interrupt();
-    }
+    helper.sendItem(result);
   }
 
   @Override
@@ -113,112 +88,11 @@ public class JSONStreamingOutputHandler extends JSONOutputHandler {
 
   @Override
   public void init() throws IOException, GateException {
-    // TODO Auto-generated method stub
-    new Thread(new StreamOutputter()).start();
+    helper.init();
   }
 
   @Override
   public void close() throws IOException, GateException {
-    try {
-      results.put(END_OF_DATA);
-    } catch(InterruptedException e) {
-      Thread.currentThread().interrupt();
-    }
+    helper.close();
   }
-
-  protected class StreamOutputter implements Runnable {
-    private File currentFile;
-
-    private int currentChunk = -1;
-
-    private OutputStream currentOutput;
-
-    private Process currentProcess;
-
-    public void run() {
-      byte[] item = null;
-      try {
-        try {
-          int bytesSinceLastCheck = 0;
-          while((item = results.take()) != END_OF_DATA) {
-            if(currentOutput == null) {
-              try {
-                openNextChunk();
-              } catch(IOException e) {
-                logger.error("Failed to open output file", e);
-                
-              }
-            }
-            try {
-              currentOutput.write(item);
-              currentOutput.flush();
-            } catch(IOException e) {
-              logger.warn("Error writing to " + currentFile.getAbsolutePath(), e);
-            }
-            bytesSinceLastCheck += item.length;
-            if(bytesSinceLastCheck > 1024 * 1024) {
-              if(currentFile.length() > chunkSize) {
-                closeChunk();
-              }
-              bytesSinceLastCheck = 0;
-            }
-          }
-        } finally {
-          closeChunk();
-          processWaiter.shutdown();
-        }
-      } catch(InterruptedException e) {
-        Thread.currentThread().interrupt();
-      }
-    }
-
-    private void closeChunk() {
-      if(currentOutput != null) {
-        try {
-          currentOutput.close();
-        } catch(IOException e) {
-          logger.warn("Error closing file " + currentFile.getAbsolutePath(), e);
-        }
-        if(currentProcess != null) {
-          final Process p = currentProcess;
-          processWaiter.execute(new Runnable() {
-            public void run() {
-              try {
-                p.waitFor();
-              } catch(InterruptedException e) {
-                logger.warn("Interrupted while waiting for process", e);
-                Thread.currentThread().interrupt();
-              }
-            }
-          });
-          currentProcess = null;
-        }
-        currentOutput = null;
-        currentFile = null;
-      }
-    }
-    
-    private void openNextChunk() throws IOException {
-      // if we're restarting we might have to skip some batches
-      do {
-        String newFileName = String.format(pattern, ++currentChunk);
-        currentFile = namingStrategy.toFile(new DocumentID(newFileName));
-      } while(currentFile.exists());
-      if(VALUE_COMPRESSION_GZIP.equals(compression)) {
-        currentOutput = new GZIPOutputStream(new FileOutputStream(currentFile));
-      } else if(compression == null || VALUE_COMPRESSION_NONE.equals(compression)) {
-        currentOutput = new FileOutputStream(currentFile);
-      } else {
-        // treat compression value as a command line
-        ProcessBuilder pb = new ProcessBuilder(compression.trim().split("\\s+"));
-        pb.directory();
-        pb.redirectInput(Redirect.PIPE);
-        pb.redirectOutput(currentFile);
-        pb.redirectError(Redirect.INHERIT);
-        currentProcess = pb.start();
-        currentOutput = currentProcess.getOutputStream();
-      }
-    }
-  }
-
 }
